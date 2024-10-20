44fe6e052aaa589e86018b3ac714c0070f216ed0
landakdu <durilandak429@gmail.com>
2024-10-20 22:30:25 +0800

    Add files via upload

---
+++ callchatuse.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
+
+function lcdewatogel() {location.replace("https://www.judisuhu.fun/chat/dewatogel/");}
+
