48d1dfcd2dc0a2d9524db9ff0c6c729c39b91d8f
landakdu <durilandak429@gmail.com>
2024-10-20 23:48:50 +0800

    Update callchatuse.js

--- callchatuse.js
+++ callchatuse.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
 
 function lcdewatogel() {location.replace("https://www.judisuhu.fun/chat/dewatogel/");}
+function lcmpo888() {location.replace("https://www.judisuhu.fun/chat/mpo888/");}
 
