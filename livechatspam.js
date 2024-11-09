4928530914ab283c48eb0b0b0ea8566faef96e2b
landakdu <durilandak429@gmail.com>
2024-11-09 18:19:19 +0800

    Add files via upload

---
+++ livechatspam.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
+
+function lcdewatogel() {location.replace("https://judisuhu.store/chat/dewatogel/");}
