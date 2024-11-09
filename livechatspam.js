b24454e9399fe9fb2eb4408091ed79f73eb4fd1d
landakdu <durilandak429@gmail.com>
2024-11-09 18:20:14 +0800

    Delete livechatspam.js

--- livechatspam.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
-
-function lcdewatogel() {location.replace("https://judisuhu.store/chat/dewatogel/");}
