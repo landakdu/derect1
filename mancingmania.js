8618ef10739373d939abecf5fe002dd06a61e58d
landakdu <durilandak429@gmail.com>
2025-11-07 11:10:44 +0800

    Delete mancingmania.js

--- mancingmania.js
+++
@@ -1,5 +0,0 @@
-function change() {location.replace("#");}
-function newtab() {window.open("#");}
-/*! Ubah yang Bawah saja */
-function link1() {window.open("https://qqresmi.fun/");}
-function link2() {window.open("");}
